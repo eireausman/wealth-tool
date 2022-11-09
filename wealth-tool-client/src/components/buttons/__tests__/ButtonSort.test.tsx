0d19bcb2285cb8c8c1b970da7a3fe6d5d15dee51
Darren Reilly <darrenreilly@Darrens-MacBook-Pro.local>
2022-11-10 09:30:21 +1000

    tests added

--- wealth-tool-client/src/components/buttons/__tests__/ButtonSort.test.tsx
+++ wealth-tool-client/src/components/buttons/__tests__/ButtonSort.test.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { act } from "react-dom/test-utils";
-import ButtonAddAsset from "../ButtonAddAsset";
 import ButtonSort from "../ButtonSort";
 
 afterEach(() => {
@@ -10,22 +9,36 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-describe("Sort Button - viewCard - test", () => {
-  it("Renders with login here message", async () => {
+describe("viewCard Sort Button", () => {
+  it("Sort button functionality", async () => {
     const clickMock = jest.fn();
     await act(async () => {
       render(
         <ButtonSort
-          sortArray={[{ readableString: "string", dbField: "string" }]}
+          sortArray={[
+            { readableString: "displayedSortString", dbField: "string" },
+          ]}
           orderByThisColumn="this"
           setorderByThisColumn={() => {}}
         />
       );
     });
-    // expect(screen.getByText("someText")).toBeInTheDocument();
-    // await act(async () => {
-    //   userEvent.click(screen.getByRole("button", { name: /sort/i }));
-    // });
-    // expect(clickMock).toHaveBeenCalledTimes(1);
+    expect(screen.getByRole("button", { name: /sort/i })).toBeInTheDocument();
+    await act(async () => {
+      fireEvent.mouseEnter(screen.getByRole("button", { name: /sort/i }));
+    });
+    expect(screen.getByText("displayedSortString")).toBeInTheDocument();
+    await act(async () => {
+      fireEvent.mouseLeave(screen.getByRole("button", { name: /sort/i }));
+    });
+    expect(screen.queryByText("displayedSortString")).not.toBeInTheDocument();
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button", { name: /sort/i }));
+    });
+    expect(screen.getByText("displayedSortString")).toBeInTheDocument();
+    await act(async () => {
+      fireEvent.click(screen.getByRole("button", { name: /sort/i }));
+    });
+    expect(screen.queryByText("displayedSortString")).not.toBeInTheDocument();
   });
 });
